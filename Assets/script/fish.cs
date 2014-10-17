6d7b1a1488e2c51fa6e2f179bd1526b542da968e
Natasha Lewandrowski <nlewandrowski@gmail.com>
2014-10-17 14:44:39 -0400

    first try at using github

---
+++ Assets/script/fish.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fish : MonoBehaviour {
+	public Vector3 destination;
+	public float speed = 1; //edit in inspector
+	public Transform food; //assign in inspector
+
+	// Use this for initialization
+	void Start () {
+		destination = food.position;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//check how close fish is to destination
+		//destination - transform.position).magnitude is basically equivolent to Vector3.Distance
+		if (Vector3.Distance (destination, transform.position) > 1f) {
+						//add force
+						//subtract position of destination - current position
+			//normalize constrains the vector to a length of 1
+			rigidbody.AddForce (Vector3.Normalize(destination - transform.position) * speed);
+			}
+		else {
+			//come to a complete stop
+			rigidbody.velocity = Vector3.zero;
+				}
+	
+	}
+}
